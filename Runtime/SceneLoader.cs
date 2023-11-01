9d0b7fc671d053ecac344bdb9fd832ceef475389
Vaclav Bily <xbily@fi.muni.cz>
2023-11-01 12:28:39 +0100

    Runtime - SceneLoader rm unsused import

--- Runtime/SceneLoader.cs
+++ Runtime/SceneLoader.cs
@@ -1,5 +1,4 @@
-﻿using System.Threading;
-using Cysharp.Threading.Tasks;
+﻿using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.ResourceManagement.ResourceProviders;
 using UnityEngine.SceneManagement;
