8e858338492da16a387abd9fac2000267bbed6a3
Vaclav Bily <xbily@fi.muni.cz>
2026-01-04 23:35:10 +0100

    Scene Dropdown Editor - Migrate to Unity 6.3 Toolbar API with new dropdown functionality

--- Editor/SceneDropdownEditor.cs
+++ Editor/SceneDropdownEditor.cs
@@ -2,24 +2,54 @@ using System.Linq;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
-using UnityToolbarExtender;
+using UnityEngine.SceneManagement;
 using Xprees.SceneManagement.Editor.Initialization;
 using Xprees.SceneManagement.ScriptableObjects;
+#if UNITY_6000_3_OR_NEWER
+using UnityEditor.Toolbars;
+
+#else
+using UnityToolbarExtender;
+#endif
 
 namespace Xprees.SceneManagement.Editor
 {
-    [InitializeOnLoad]
     public static class SceneDropdownEditor
     {
-        private readonly static EditorSceneTracker sceneTracker;
+        private readonly static EditorSceneTracker sceneTracker = new();
 
+#if UNITY_6000_3_OR_NEWER
         static SceneDropdownEditor()
         {
-            sceneTracker = new EditorSceneTracker();
-            ToolbarExtender.LeftToolbarGUI.Add(OnToolbarGUI);
+            SceneManager.activeSceneChanged += (_, _) => RefreshSceneList();
+            EditorApplication.projectChanged += RefreshSceneList;
+            EditorSceneManager.activeSceneChangedInEditMode += (_, _) => RefreshSceneList();
         }
 
-        #region GUI
+        private const string toolbarSceneDropdownPath = "Scenes/Scene Selector";
+
+        [MainToolbarElement(toolbarSceneDropdownPath, defaultDockPosition = MainToolbarDockPosition.Left)]
+        public static MainToolbarElement RenderSceneDropdown()
+        {
+            var content = new MainToolbarContent(
+                GetTitle().text,
+                EditorGUIUtility.IconContent("BuildSettings.Editor").image as Texture2D,
+                "Manage scenes");
+
+            return new MainToolbarDropdown(content, ShowSceneDropDownMenu);
+        }
+
+        private static void ShowSceneDropDownMenu(Rect dropdownRect)
+        {
+            PrepareSceneListMenu().DropDown(dropdownRect);
+            MainToolbar.Refresh(toolbarSceneDropdownPath);
+        }
+#else
+        [InitializeOnLoadMethod]
+        public static void RenderToolbarTools()
+        {
+            ToolbarExtender.LeftToolbarGUI.Add(OnToolbarGUI);
+        }
 
         private static void OnToolbarGUI()
         {
@@ -28,55 +58,64 @@ namespace Xprees.SceneManagement.Editor
             // Create the dropdown button
             if (GUILayout.Button(GetTitle(), EditorStyles.toolbarDropDown))
             {
-                var menu = new GenericMenu();
-
-                menu.AddItem(new GUIContent("Refresh"), false, sceneTracker.RefreshList);
-                menu.AddSeparator("");
-
-                var isLoadedInitScene = EditorSceneLoader.IsLoadedInitScene;
-                menu.AddItem(new GUIContent("Load Init Scene"), isLoadedInitScene,
-                    async () => await EditorSceneLoader.ToggleLoadOrUnloadInitScene(OpenSceneMode.Additive));
-
-                var editorInitializationActive = EditorInitializationLoader.Active;
-                menu.AddItem(new GUIContent("Editor PlayMode Init (Normally on)"), editorInitializationActive,
-                    () => EditorInitializationLoader.Active = !editorInitializationActive);
-
-                menu.AddSeparator("");
-                menu.AddItem(new GUIContent("Unload all scenes (except init)"), false, UnloadAllScenesExceptInit);
-                menu.AddSeparator("");
-
-                menu.AddDisabledItem(new GUIContent("Scenes"));
-                menu.AddSeparator("");
-
-                var previewSceneType = SceneType.PersistentManagers;
-                foreach (var scene in sceneTracker.Scenes.OrderByDescending(scene => scene.sceneType))
-                {
-                    var isSceneOpenOrPresent = sceneTracker.IsSceneOpen(scene);
-                    if (isSceneOpenOrPresent == null) continue;
-
-                    if (scene.sceneType != previewSceneType)
-                    {
-                        previewSceneType = scene.sceneType;
-                        menu.AddSeparator("");
-                    }
-
-                    var hasValidSceneReference = scene.HasValidSceneReference();
-                    if (!hasValidSceneReference)
-                    {
-                        menu.AddDisabledItem(new GUIContent($"{scene.sceneName} ({scene.sceneType}) - Invalid Scene!"));
-                        continue;
-                    }
-
-                    menu.AddItem(new GUIContent($"{scene.sceneName} ({scene.sceneType})"), isSceneOpenOrPresent!.Value, MenuFunction);
-                    continue;
-
-                    void MenuFunction() => ProcessScene(scene);
-                }
-
+                var menu = PrepareSceneListMenu();
                 menu.ShowAsContext();
             }
 
             GUILayout.FlexibleSpace();
+        }
+#endif
+
+
+        /// Prepares the scene list menu for the dropdown
+        private static GenericMenu PrepareSceneListMenu()
+        {
+            var menu = new GenericMenu();
+
+            menu.AddItem(new GUIContent("Refresh"), false, RefreshSceneList);
+            menu.AddSeparator("");
+
+            var isLoadedInitScene = EditorSceneLoader.IsLoadedInitScene;
+            menu.AddItem(new GUIContent("Load Init Scene"), isLoadedInitScene,
+                async () => await EditorSceneLoader.ToggleLoadOrUnloadInitScene(OpenSceneMode.Additive));
+
+            var editorInitializationActive = EditorInitializationLoader.Active;
+            menu.AddItem(new GUIContent("Editor PlayMode Init (Normally on)"), editorInitializationActive,
+                () => EditorInitializationLoader.Active = !editorInitializationActive);
+
+            menu.AddSeparator("");
+            menu.AddItem(new GUIContent("Unload all scenes (except init)"), false, UnloadAllScenesExceptInit);
+            menu.AddSeparator("");
+
+            menu.AddDisabledItem(new GUIContent("Scenes"));
+            menu.AddSeparator("");
+
+            var previewSceneType = SceneType.PersistentManagers;
+            foreach (var scene in sceneTracker.Scenes.OrderByDescending(scene => scene.sceneType))
+            {
+                var isSceneOpenOrPresent = sceneTracker.IsSceneOpen(scene);
+                if (isSceneOpenOrPresent == null) continue;
+
+                if (scene.sceneType != previewSceneType)
+                {
+                    previewSceneType = scene.sceneType;
+                    menu.AddSeparator("");
+                }
+
+                var hasValidSceneReference = scene.HasValidSceneReference();
+                if (!hasValidSceneReference)
+                {
+                    menu.AddDisabledItem(new GUIContent($"{scene.sceneName} ({scene.sceneType}) - Invalid Scene!"));
+                    continue;
+                }
+
+                menu.AddItem(new GUIContent($"{scene.sceneName} ({scene.sceneType})"), isSceneOpenOrPresent!.Value, MenuFunction);
+                continue;
+
+                void MenuFunction() => ProcessScene(scene);
+            }
+
+            return menu;
         }
 
         private async static void UnloadAllScenesExceptInit()
@@ -97,8 +136,6 @@ namespace Xprees.SceneManagement.Editor
         private static GUIContent GetTitle() =>
             new("Scenes", EditorGUIUtility.IconContent("BuildSettings.Editor").image);
 
-        #endregion
-
         private static void ProcessScene(SceneSO scene)
         {
             if (sceneTracker.IsSceneOpen(scene) ?? false)
@@ -108,6 +145,14 @@ namespace Xprees.SceneManagement.Editor
             }
 
             EditorSceneLoader.OpenScene(scene);
+        }
+
+        private static void RefreshSceneList()
+        {
+            sceneTracker.RefreshList();
+#if UNITY_6000_3_OR_NEWER
+            MainToolbar.Refresh(toolbarSceneDropdownPath); // Refresh the toolbar to reflect any changes
+#endif
         }
     }
 }
